c3909c83c24780cfe4e5427aaa7db83b055ed555
Adam Oresten <adamoresten@gmail.com>
2021-06-02 20:24:30 +0100

    Straight up steal AA's implementations

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,29 @@
+import { EffectsLayer } from "./effects-layer.js";
+
+function registerLayer() {
+    if (game.data.version === "0.7.9" || game.data.version === "0.7.10") {
+        const layers = mergeObject(Canvas.layers, {
+            autoanimations: EffectsLayer
+        });
+        Object.defineProperty(Canvas, 'layers', {
+            get: function () {
+                return layers
+            }
+        });
+    } else {
+        const layers = foundry.utils.mergeObject(Canvas.layers, {
+            autoanimations: EffectsLayer
+        });
+        Object.defineProperty(Canvas, 'layers', {
+            get: function () {
+                return layers
+            }
+        });
+    }
+}
+
 Hooks.once('init', async function() {
 
-});
-
-Hooks.once('ready', async function() {
+    registerLayer();
 
 });
